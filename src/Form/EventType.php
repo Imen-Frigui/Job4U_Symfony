3d35e839840435c79ffe1baa37b8c0d4997004ee
Imen-Frigui <89009627+Imen-Frigui@users.noreply.github.com>
2023-04-09 18:43:11 +0100

    FontOffice

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Event;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,7 @@ class EventType extends AbstractType
     {
         $builder
             ->add('title')
-            ->add('description')
+            ->add('description',TextareaType::class)
             ->add('date')
             ->add('location')
         ;
